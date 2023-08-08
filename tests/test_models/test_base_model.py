b3558a57577d9b7192123dffd2a3e7cf5169f6f6
Toteya <111172685+Toteya@users.noreply.github.com>
2023-08-08 08:54:27 +0200

    Empty base_model module

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+test_base_model module
+tests the base_model module from the models package
+"""
+import unittest
+from models import base_model
+
+class TestBaseModel(unittest.TestCase):
+    """ Tests the BaseModel class
+    """
+
+    def test_BaseModel(self):
+        """ Tests the instantiation of a BaseModel instance
+        """
+        pass
