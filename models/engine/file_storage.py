6826ee0ecf1ab59b20f7a00a79d2a2516c514ce4
bonheurdev <125626137+bonheurdev@users.noreply.github.com>
2023-08-09 19:16:15 +0200

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,15 +1,16 @@
+#!/usr/bin/python3
 """
 file_storage module
 Contains the FileStorage class
 """
 import json
-# import models.base_model
+
 
 class FileStorage:
     """
     FileStorage class
     Serializes instances to a JSON file and deserializes JSON file to instances
-    
+
     Attributes:
         __file_path (str): Path to the JSON file (e.g. file.json)
         __objects (dict): Will store all objects by <class name>.id
@@ -17,7 +18,6 @@ class FileStorage:
 
     __file_path = "file.json"
     __objects = {}
-
 
     def all(self):
         """ Returns the dictionary `__objects`
@@ -34,22 +34,25 @@ class FileStorage:
     def save(self):
         """ Serializes __objects to the JSON file (path: __file_path)
         """
+        json_dict = {}
+        for key, value in self.__objects.items():
+            json_dict[key] = value.to_dict()
+
         with open(self.__file_path, "w", encoding="utf-8") as file:
-            objs = []
-            for key, value in self.__objects.items():
-                objs.append(value.to_dict())
-            json.dump(objs, file)
+            json.dump(json_dict, file)
 
     def reload(self):
         """ Deserializes the JSON file to __objects if the JSON
         file (__file_path) exits.
         """
-        objs = []
+        import models.base_model as bm
+
+        json_dict = {}
         try:
             with open(self.__file_path, "r", encoding="utf-8") as file:
-                objs = json.load(file)
+                json_dict = json.load(file)
         except FileNotFoundError:
             pass
 
-        # for obj in objs:
-        #    base_model.BaseModel(obj)
+        for obj in json_dict.values():
+            self.new(bm.BaseModel(**obj))
