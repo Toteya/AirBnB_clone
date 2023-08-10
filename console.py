85de355c9d7d36362c1d5e5a71821c1952f03448
Toteya <111172685+Toteya@users.noreply.github.com>
2023-08-10 19:05:06 +0200

    Updated console: do_update method + help methods

--- console.py
+++ console.py
@@ -19,22 +19,26 @@ class HBNBCommand(cmd.Cmd):
     classes = ['BaseModel']
 
     def do_all(self, class_name):
-        """ Prints string representations of all instances based on the class
+        """ Prints a string representation of all instances based on the class
         name. If no class name is provided all instances of all classes are
         printed.
         """
-        if not class_name:
-            for obj in models.storage.all().values():
-                print(obj)
-        elif class_name in self.classes:
-            for obj in models.storage.all().values():
-                if obj.__class__.__name__ == class_name:
-                    print(obj)
-        else:
+        str_objs = []
+
+        if class_name and class_name not in self.classes:
             print("** class doesn't exist **")
+        else:
+            for obj in models.storage.all().values():
+                if not class_name or obj.__class__.__name__ == class_name:
+                    str_objs.append(str(obj))
+            print(str_objs)
 
     def help_all(self):
-        pass
+        help_str = "\n".join(["Prints a string representation of all "+
+                              "instancesbased on the class",
+                              "If no class name is provided all instances "+
+                              "of all classes are printed."])
+        print(help_str)
 
 
     def do_create(self, class_name):
@@ -51,7 +55,9 @@ class HBNBCommand(cmd.Cmd):
             print(bm.id)
 
     def help_create(self):
-        pass
+        help_str = "\n".join(["Creates a new instance of BaseModel, "+
+                              "saves it to the file, and prints its id"])
+        print(help_str)
 
     def do_destroy(self, line):
         """ Deletes an instance based on the class name and the id
@@ -80,7 +86,8 @@ class HBNBCommand(cmd.Cmd):
             models.storage.save()
 
     def help_destroy():
-        pass
+        help_str = "Deletes an instance based on the class name and the id"
+        print(help_str)
 
     def do_EOF(self, line):
         """Exits the programm
@@ -88,7 +95,8 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def help_EOF(self):
-        pass
+        help_str = "Exits the program"
+        print(help_str)
 
     def do_quit(self, line):
         """Exits the programm
@@ -96,7 +104,8 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def help_quit(self):
-        pass
+        help_str = "Exits the program"
+        print(help_str)
 
     def do_show(self, line):
         """ Prints the string representation of an instance based on the
@@ -124,13 +133,45 @@ class HBNBCommand(cmd.Cmd):
             print(obj)
 
     def help_show(self):
-        pass
+        help_str = "\n".join(['Prints the string reprensation of an instance '+
+                              'based on the class name and the id',
+                              'Usage: show <class name> <id>]'])
+        print(help_str)
 
     def do_update(self, line):
         """ Updates an instance based on the class name and id by adding or
         updating the attribute, and save the change to the file.
         """
-        pass
+        class_name = ""
+        obj_id = ""
+        attr_name = ""
+        attr_value = ""
+
+        args = line.split()
+        try:
+            class_name = args[0]
+            obj_id = args[1]
+            attr_name = args[2]
+            attr_value = args[3]
+        except IndexError:
+            pass
+
+        if not class_name:
+            print("** class name missing **")
+        elif class_name not in self.classes:
+            print("** class doesn't exist **")
+        elif not obj_id:
+            print("** instance id missing **")
+        elif f'{class_name}.{obj_id}' not in models.storage.all():
+            print("** no instance found **")
+        elif not attr_name:
+            print("** attribute name missing **")
+        elif not attr_value:
+            print("** value missing **")
+        else:
+            obj = models.storage.all()[f'{class_name}.{obj_id}']
+            if hasattr(obj, attr_name):
+                setattr(obj, attr_name, attr_value)
 
     def help_update(self):
         pass
