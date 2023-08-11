39df41279dd7d154f0133803b6bb4669532a1891
Toteya <111172685+Toteya@users.noreply.github.com>
2023-08-11 14:30:11 +0200

    Updated console and file storage to accomodate the new classes

--- console.py
+++ console.py
@@ -9,6 +9,11 @@ import sys
 import models
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
@@ -20,6 +25,11 @@ class HBNBCommand(cmd.Cmd):
     classes = {
             'BaseModel': BaseModel,
             'User': User
+            'State': State,
+            'City': City,
+            'Amenity': Amenity,
+            'Place': Place,
+            'Review': Review
         }
 
     def __init__(self):
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -46,10 +46,20 @@ class FileStorage:
         """
         from models.base_model import BaseModel
         from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
 
         classes = {
                 'BaseModel': BaseModel,
-                'User': User
+                'User': User,
+                'State': State,
+                'City': City,
+                'Amenity': Amenity,
+                'Place': Place,
+                'Review': Review
             }
 
         json_dict = {}
