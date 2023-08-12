a9b8cd55f2f43b2b5a2b869f494ac524d5c262c5
bonheurdev <125626137+bonheurdev@users.noreply.github.com>
2023-08-12 17:15:04 +0200

    state

---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module for state class"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """the state
+
+    Attributes:
+        name (str): state's name       
+    """
+
+    name = ""
