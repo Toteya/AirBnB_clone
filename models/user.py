3731ecd2e78b903aedc244757235140ac9574dd3
Toteya <111172685+Toteya@users.noreply.github.com>
2023-08-12 20:46:30 +0200

    updated User class: only initialise the attributes provided in **kwargs

--- models/user.py
+++ models/user.py
@@ -13,10 +13,12 @@ class User(BaseModel):
     Attributes:
     """
 
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
 
+    """
     def __init__(self, *args, **kwargs):
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
         super().__init__(**kwargs)
+    """
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -14,27 +14,42 @@ class TestUser(TestCase):
     def setUp(self):
         """ Sets up the initial conditions for each test case
         """
-        pass
+        self.user1 = User();
+
+    def tearDown(self):
+        """ Tears down tests conditions and objects after each test
+        """
+        del self.user1
 
     def test_User(self):
         """ Tests the instantiation of a User instance
         """
-        user1 = User()
+        self.assertIsInstance(self.user1.id, str)
+        self.assertIsNotNone(self.user1.created_at)
+        self.assertIsNotNone(self.user1.updated_at)
 
-        self.assertIsInstance(user1.id, str)
-        self.assertIsNotNone(user1.created_at)
-        self.assertIsNotNone(user1.updated_at)
+        self.assertEqual("", self.user1.email)
+        self.assertEqual("", self.user1.password)
+        self.assertEqual("", self.user1.first_name)
+        self.assertEqual("", self.user1.last_name)
 
-        self.assertEqual("", user1.email)
-        self.assertEqual("", user1.password)
-        self.assertEqual("", user1.first_name)
-        self.assertEqual("", user1.last_name)
-
-        user2 = User(**user1.to_dict())
-        self.assertEqual(user1.id, user2.id)
+        dict2 = {
+                'id': 'cac66cd4-bd96-4161-b54a-a208e2d59534',
+                'created_at': '2019-05-28T21:03:54.052298',
+                'updated_at': '2019-05-28T21:03:54.052301',
+                'email': 'gigi@kmail.na',
+                'first_name': 'Hagrid'
+            }
+        user2 = User(**dict2)
+        self.assertEqual(user2.id, 'cac66cd4-bd96-4161-b54a-a208e2d59534')
+        # self.assertEqual(user2.created_at, '2019-05-28T21:03:54.052298')
+        # self.assertEqual(user2.updated_at, '2019-05-28T21:03:54.052301')
+        self.assertEqual(user2.email, 'gigi@kmail.na')
+        self.assertEqual(user2.first_name, 'Hagrid')
+        self.assertEqual(user2.last_name, "")
+        self.assertNotIn('last_name', user2.to_dict())
 
     def test_str(self):
         """ Tests the string representation of a User instance
         """
         pass
-
