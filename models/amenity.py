ee41208f5227ebc03c7b16859a502ec22be23bf5
bonheurdev <125626137+bonheurdev@users.noreply.github.com>
2023-08-12 17:12:08 +0200

    amenity

---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""module for amenity class"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """amenity representation.
+
+    Attributes:
+        name (str): amenit's name      
+    """
+
+    name = ""
