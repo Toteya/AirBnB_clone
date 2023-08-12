9a18923034cc7b9f817f0d9bf4c4957388ae3588
bonheurdev <125626137+bonheurdev@users.noreply.github.com>
2023-08-12 17:12:49 +0200

    city

---
+++ models/city.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module for city class"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """city class.
+
+    Attributes:
+        state_id (str): state id.
+        name (str): city's name.
+    """
+
+    state_id = ""
+    name = ""
