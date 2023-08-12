d83d52b08edbdd5e58a349105c8dfc010662b7de
bonheurdev <125626137+bonheurdev@users.noreply.github.com>
2023-08-12 17:14:31 +0200

    review

---
+++ models/review.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module for review class"""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """represent about the reviews     
+
+    Attributes:
+        place_id (str): id of the place
+        user_id (str): User id.        
+        text (str): review text        
+    """
+
+    place_id = ""
+    user_id = ""
+    text = ""
